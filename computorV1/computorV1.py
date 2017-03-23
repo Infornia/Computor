db1c7413036805469c21527228cc55905c34c87f
Michael WILK <mwilk@e2r9p1.42.fr>
2017-03-23 16:42:19 +0100

    first push

---
+++ computorV1/computorV1.py
@@ -0,0 +1,10 @@
+import math
+import sys
+
+if len(sys.argv) > 2:
+	print("Too many arguments Bro")
+elif len(sys.argv) == 1:
+	print("Usage: ./computor \"5 * X^0 + 4 * X^1 - 9.3 * X^2 = 1 * X^0\"")
+else:
+	print("Welcome to computorV1")
+	
