ad15b0f381153057675487ce357b2f6fa7a7db2a
Michael WILK <mwilk@e2r9p1.42.fr>
2017-03-23 18:36:54 +0100

    secound push

--- computorV1/computorV1.py
+++ computorV1/computorV1.py
@@ -1,4 +1,6 @@
+#!/usr/bin/python
 import math
+import re
 import sys
 
 if len(sys.argv) > 2:
@@ -7,4 +9,13 @@ elif len(sys.argv) == 1:
 	print("Usage: ./computor \"5 * X^0 + 4 * X^1 - 9.3 * X^2 = 1 * X^0\"")
 else:
 	print("Welcome to computorV1")
-	
+	split = re.split("=" ,sys.argv[1])
+	print(split)
+	if (len(split) != 2):
+		print("Bad boy, missing equality")
+	else:
+		a = []
+		for i in range(len(split)):
+			a.append([x.strip() for x in re.split('(\+|\-)+', split[i])])
+		print(a)
+		
