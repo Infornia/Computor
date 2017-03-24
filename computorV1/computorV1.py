35f9a8c27d40fac6c7ae89ee6f95e86e8e4a0e60
Michael WILK <mwilk@e2r11p3.42.fr>
2017-03-24 18:47:54 +0100

    secound push

--- computorV1/computorV1.py
+++ computorV1/computorV1.py
@@ -9,13 +9,15 @@ elif len(sys.argv) == 1:
 	print("Usage: ./computor \"5 * X^0 + 4 * X^1 - 9.3 * X^2 = 1 * X^0\"")
 else:
 	print("Welcome to computorV1")
-	split = re.split("=" ,sys.argv[1])
+	split = re.split("(=?)" ,sys.argv[1])
 	print(split)
-	if (len(split) != 2):
-		print("Bad boy, missing equality")
+	if (len(split) != 3):
+		print("Bad Syntax")
 	else:
 		a = []
 		for i in range(len(split)):
-			a.append([x.strip() for x in re.split('(\+|\-)+', split[i])])
+			a.append(re.split('\s*([+-=]?[0-9\.]+)\s*([*])\s*(X\^\d)\s*', split[i]))
 		print(a)
+		for i in range(len(split)):
+			print(a[i])
 		
