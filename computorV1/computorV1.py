f4cc83c5db7fe165f29ef5dd6acc79766c51c795
Michael WILK <mwilk@e1r1p7.42.fr>
2017-04-03 15:09:24 +0200

    push comp

--- computorV1/computorV1.py
+++ computorV1/computorV1.py
@@ -4,7 +4,7 @@ import re
 import sys
 
 re_space = compile('\s+')
-re_pol = compile('([-+=]?)\s*([0-9\.]+)?(\s*\*?\s*[xX](?:\s*\^\s*([0-9]+))?)?\s*')
+re_pol = compile('([-+]?)\s*([0-9\.]+)?(\s*\*?\s*[X](?:\s*\^\s*([0-9]+))?)?\s*')
 re_comp2 = compile('([+-]?[0-9\.]+)\s*([*])\s*(X\^\d)\s*')
 
 class Polynom():
@@ -17,7 +17,7 @@ class Polynom():
 	def __init__(self, m, sign = None, n = 0.0, x = False, pow = 0):
 		self.sign = sign
 		self.n = n
-		self.x = x 
+		self.x = x
 		self.pow = pow
 		if m != None:
 			if len(m.group(1)) > 0:
@@ -38,31 +38,28 @@ class Polynom():
 		if self.pow == 0 and self.x:
 			self.x = False
 
-class Computor():
-	def __init__(self):
-		self.left = []
-		self.right = []
-		
-	def parsing(self, eq):
-		i = 0
-		while i < len(eq):
-			m = re_space.match(eq, i)
-			if m != None:
-				i += len(m.group(0))
-				continue
-			m = re_pol.match(eq, i)
-			print(m.group())
-			if m == None or len(m.group(0)) <= 0:
-				print("\033[31m Strange Syntax Bro 1: '%s' \033[39m" % (eq[i:i+5]))
-				return False
-			try:
-				p = Polynom(m)
-			except:
-				print("\033[31m Strange Syntax Bro 2: '%s' \033[39m" % (eq[i:i+5]))
-				return False
-				
-				
-			i +=1
+
+def parse(eq):
+	i = 0
+	ret = []
+	while i < len(eq):
+		match = re_space.match(eq, i)
+		if match != None:
+			i += len(match.group(0))
+			continue
+		match = re_pol.match(eq, i)
+		print(match.group())
+		if match == None or len(match.group(0)) <= 0:
+			print("\033[31m Strange Syntax Bro 1: '%s' \033[39m" % (eq[i:i+5]))
+			return False
+		try:
+			p = Polynom(match)
+		except:
+			print("\033[31m Strange Syntax Bro 2: '%s' \033[39m" % (eq[i:i+5]))
+			return False
+		i += len(match.group(0))
+		ret.append(p)
+	return ret
 
 if len(sys.argv) > 2:
 	print("Too many arguments Bro")
@@ -74,6 +71,10 @@ else:
 	if (len(split) != 3):
 		print("Bad Syntax")
 	else:
-		c = Computor(split[0], split[2])
-		c.parsing(split[0])
-		print(vars(c))
+		left = []
+		right = []
+		left.append(parse(split[0]))
+		right.append(parse(split[2]))
+		print(left, right)
+		for i in left:
+			print(i.sign, i.n, i.x, i.pow)
